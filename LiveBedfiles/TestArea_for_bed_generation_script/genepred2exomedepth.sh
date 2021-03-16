c11694590d737ea521970522d860607ded672761
Aled Jones <aledjones@nhs.net>
2021-03-16 15:17:21 +0000

    add comments

--- LiveBedfiles/TestArea_for_bed_generation_script/genepred2exomedepth.sh
+++ LiveBedfiles/TestArea_for_bed_generation_script/genepred2exomedepth.sh
@@ -3,7 +3,14 @@ REFGENE=$1
 PADDING=$2
 
 # BED files with exon numbers for exomedepth
-#echo "Building ExomeDepth exons..."
+# Refseq files have one line per gene (see readme for an example refseq line)
+# the below loop goes through each line and:
+# takes the columns exonStarts and exonSEnds and splits into new lists astarts and aends for each gene
+# for each exon the strand is assessed and the exon count is calculated
+# if minus strand then the exon numbers are subtracted from exon count, else just take the exon count
+# the chromosome, start (minus padding), stop (plus padding) and gene_exonNumber printed to screen
+
+# set file seperation
 awk -v padding=${PADDING} '
 BEGIN { OFS = "\t" };
 {
